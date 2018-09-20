0c74b21e6d214ff0811ed36276caf65f87631175
lolcapm <amayberc@gmail.com>
2018-09-20 19:14:20 -0400

    Parctica MArio Bross

---
+++ Practica 2 Segundo Parcial IA/Assets/Scripts/Destruccion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruccion : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Cabeza"))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Practica 2 Segundo Parcial IA/Assets/Scripts/DieMario.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieMario : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Choque"))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
